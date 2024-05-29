08378460c43a386cbb0de3f61b10a1306a1ae33f
lumzyj <lumzkid@gmail.com>
2024-05-29 03:13:14 +0100

    first commit

--- app.js
+++ app.js
@@ -1,55 +1,50 @@
 const form = document.getElementById('form');
-const button = document.getElementById('button');
 const firstName = document.querySelector('.firstName');
 const lastName = document.querySelector('.lastName');
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
 
-console.log(firstName);
-
-// console.log(firstName, lastName, email, password);
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const fName = firstName.value;
-  const lName = lastName.value;
-  const emailVal = email.value;
-  const passwordVal = password.value;
-  console.log(fName, lName, emailVal, passwordVal);
 
-  // Check first name
-  if (fName === '') {
-    firstName.classList.add('error');
-  } else {
-    firstName.classList.remove('error');
-  }
-  // Check last name
+  const fields = [
+    { field: firstName, value: firstName.value, errorMsg: 'First Name cannot be empty' },
+    { field: lastName, value: lastName.value, errorMsg: 'Last Name cannot be empty' },
+    { field: email, value: email.value, errorMsg: 'Email cannot be empty', validate: validateEmail, invalidMsg: 'Looks like this is not an email' },
+    { field: password, value: password.value, errorMsg: 'Password cannot be empty' }
+  ];
 
-  if (lName === '') {
-    lastName.classList.add('error');
-  } else {
-    lastName.classList.remove('error');
-  }
-  // Check email
-
-  if (!validateEmail(emailVal) || emailVal === '') {
-    email.classList.add('error');
-  } else {
-    email.classList.remove('error');
-  }
-
-  // Check password
-
-  if (passwordVal === '') {
-    password.classList.add('error');
-  } else {
-    password.classList.remove('error');
-  }
+  fields.forEach(({ field, value, errorMsg, validate, invalidMsg }) => {
+    if (value === '') {
+      showError(field, errorMsg);
+    } else if (validate && !validate(value)) {
+      showError(field, invalidMsg);
+    } else {
+      showSuccess(field);
+    }
+  });
 });
 
-//Validate email
+function showError(input, message) {
+  input.classList.add('error');
+  let errorElement = input.nextElementSibling;
+  if (!errorElement || !errorElement.classList.contains('error-message')) {
+    errorElement = document.createElement('small');
+    errorElement.classList.add('error-message');
+    input.parentNode.insertBefore(errorElement, input.nextSibling);
+  }
+  errorElement.textContent = message;
+}
+
+function showSuccess(input) {
+  input.classList.remove('error');
+  const errorElement = input.nextElementSibling;
+  if (errorElement && errorElement.classList.contains('error-message')) {
+    errorElement.remove();
+  }
+}
+
 function validateEmail(email) {
-  var re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
